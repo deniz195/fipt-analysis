f985ea21f0294ffbdaee37cacb8ebc1c5deabd8e
Eric Williams <ewilliams@battrion.com>
2020-03-18 16:12:07 +0100

    Parameterized chisqr test to run as an individual test on each data file.

--- tests/test_fipt.py
+++ tests/test_fipt.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import pytest
 # get module from parent directory
 import os,sys,inspect
 from pathlib import Path
@@ -9,9 +9,12 @@ sys.path.insert(0, parent_dir)
 
 import fipt
 
-def test_full_fit():
+def gather_filenames():
+    p = Path(current_dir).joinpath('data')
+    filenames = list(p.glob('**/*.csv'))
+    return filenames
 
-    fn = str(Path(current_dir).joinpath('test_data_001.csv'))
+def fit_filename(fn):
     df = pd.read_csv(fn)
     df.head()
 
@@ -52,58 +55,15 @@ def test_full_fit():
 
     symimfit.save_results()
 
-
-def test_many_full_fits():
-
-    failed_fits = []
-    p = Path(current_dir).joinpath('data')
-    # print(list(p.glob('**/*.csv')))
-    for fn in list(p.glob('**/*.csv')):
-    # fn = str(Path(current_dir).joinpath('test_data_001.csv'))
-        fn = str(fn)
-        df = pd.read_csv(fn)
-        df.head()
-
-        ipdata =  fipt.ImpedanceData(fn, fn, 
-                           f_data = df['Frequency (Hz)'].values,  
-                           z_real_data = df['Z\' (Ohms)'].values, 
-                           z_imag_data = df['Z\" (Ohms)'].values)
+    return result
 
 
-        symimfit = fipt.SymmetricImpedanceFitter(impedance_data=ipdata)        
-        symimfit.sanitize_data()
+def test_full_fit():
+    fn = str(Path(current_dir).joinpath('test_data_001.csv'))
+    fit_filename(fn)
 
-        # restrict data range
-        symimfit.set_min_w(None)
-        symimfit.set_max_z_abs(400)
 
-        # use student t likelihood function
-        symimfit.configure_likelihood(likelihood_config=dict(name='t', scale=1, df=1))
-
-        # guess start parameters
-        start_params = symimfit.guess(make_plots=False)
-
-        # manual configuration of parameters
-        start_params_1 = start_params
-        # start_params_1['gamma'].set(value=0.916)
-        # start_params_1['r_ion'].set(value=50)
-        # start_params_1['q_s'].set(value=0.004)
-
-        # symimfit.plot_fit(params=start_params_1);        
-        #         symimfit.set_max_z_abs(250)
-
-        result = symimfit.fit()
-
-        fit_report_str = fipt.lmfit.fit_report(result, show_correl=False)
-        # print(fit_report_str)
-
-        f, ax = symimfit.plot_fit(start_params = start_params_1);
-        # f.show()
-
-        symimfit.save_results()
-
-        if result.chisqr > 200:
-            failed_fits.append((fn,result.chisqr))
-
-    assert_str = ''.join("\n File={} \t chisqr={}".format(a,b) for a,b in failed_fits)
-    assert len(failed_fits) == 0, assert_str
+@pytest.mark.parametrize('fn', gather_filenames())
+def test_chisqr(fn):
+    result = fit_filename(str(fn))
+    assert result.chisqr < 200
