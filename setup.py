5086363308bc292e3b7d54d454951d7a2f1e91d5
Deniz Bozyigit <dbozyigit@battrion.com>
2020-03-23 13:33:07 +0100

    changed version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='fipt',
-    version='0.1.1',
+    version='0.1.2dev',
     description='A python module to analyze fast impedance tortuosity measurements.',
     long_description=readme,
     long_description_content_type='text/markdown',   
